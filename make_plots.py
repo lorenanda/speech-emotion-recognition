11c6bc31e6db6b1dd071fb57984f09f0710be925
lorenanda <lorena.ciutacu@gmail.com>
2020-12-10 13:27:37 +0100

    add function make_plots

---
+++ make_plots.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+def make_plots():
+    fig_dims = (10, 7)
+    fig, ax = plt.subplots(figsize=fig_dims)
+
+    df = pd.read_csv("features/speech_features.csv")
+
+    plot_emotions = sns.countplot(
+        x="emotion", data=df, color="lightseagreen", ax=ax
+    ).set_title("RAVDESS Audio Dataset")
+    plot_emotions.figure.savefig("images/plot_emotions.png")
+    plot_intensity = sns.countplot(
+        x="intensity", data=df, color="lightseagreen", ax=ax
+    ).set_title("RAVDESS Audio Dataset")
+    plot_intensity.figure.savefig("images/plot_intensity.png")
+    plot_gender = sns.countplot(
+        x="gender", data=df, color="lightseagreen", ax=ax
+    ).set_title("RAVDESS Audio Dataset")
+    plot_gender.figure.savefig("images/plot_gender.png")
+
+    print("Successfully created plots.")
+
+
+if __name__ == "__main__":
+    make_plots()
