d9e90c7c1a48388f4c326caf288a5cd485b55853
lorenanda <lorena.ciutacu@gmail.com>
2020-12-11 17:44:29 +0100

    change extract_features function

--- speech_emotion_recognition/extract_features.py
+++ speech_emotion_recognition/extract_features.py
@@ -9,44 +9,47 @@ def extract_features(PATH):
     emotions_list = []
     intensity_list = []
 
-    for item in PATH:
-        if item[7] == "1":
-            emotions_list.append("neutral")
-        elif item[7] == "2":
-            emotions_list.append("calm")
-        elif item[7] == "3":
-            emotions_list.append("happy")
-        elif item[7] == "4":
-            emotions_list.append("sad")
-        elif item[7] == "5":
-            emotions_list.append("angry")
-        elif item[7] == "6":
-            emotions_list.append("fearful")
-        elif item[7] == "7":
-            emotions_list.append("disgusted")
-        elif item[7] == "8":
-            emotions_list.append("surprised")
+    for dir, _, files in os.walk(PATH):
+        for item in files:
+            if int(item[7]) == "1":
+                emotions_list.append("neutral")
+            elif item[7] == "2":
+                emotions_list.append("calm")
+            elif item[7] == "3":
+                emotions_list.append("happy")
+            elif item[7] == "4":
+                emotions_list.append("sad")
+            elif item[7] == "5":
+                emotions_list.append("angry")
+            elif item[7] == "6":
+                emotions_list.append("fearful")
+            elif item[7] == "7":
+                emotions_list.append("disgusted")
+            elif item[7] == "8":
+                emotions_list.append("surprised")
 
-        if int(item[-6:-4]) % 2 == 0:
-            gender_list.append("female")
-        elif int(item[-6:-4]) % 2 != 0:
-            gender_list.append("male")
+            if int(item[-6:-4]) % 2 == 0:
+                gender_list.append("female")
+            elif int(item[-6:-4]) % 2 != 0:
+                gender_list.append("male")
 
-        if item[10] == "1":
-            intensity_list.append("normal")
-        elif item[10] == "2":
-            intensity_list.append("strong")
+            if item[10] == "1":
+                intensity_list.append("normal")
+            elif item[10] == "2":
+                intensity_list.append("strong")
 
-        files_list.append(item)
+            files_list.append(item)
 
-    df = pd.DataFrame()
-    df["file"] = files_list
-    df["gender"] = gender_list
-    df["emotion"] = emotions_list
-    df["intensity"] = intensity_list
+        df = pd.DataFrame()
+        df["file"] = files_list
+        df["gender"] = gender_list
+        df["emotion"] = emotions_list
+        df["intensity"] = intensity_list
 
-    df.to_csv("features/df_features.csv", index=False)
+        df.to_csv("features/df_features_new.csv", index=False)
 
 
-PATH = os.listdir("data/")
-extract_features(PATH)
+if __name__ == "__main__":
+    # PATH = os.listdir("data/")
+    # PATH = os.walk("data/")
+    extract_features("data/")
