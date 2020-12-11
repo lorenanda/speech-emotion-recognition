1955a2f40379eee7bb752bcefda7d592328713e3
lorenanda <lorena.ciutacu@gmail.com>
2020-12-11 19:18:20 +0100

    add paths vars

---
+++ paths.py
@@ -0,0 +1,4 @@
+DATA_PATH = "speech_emotion_recognition/data"
+FEATURES_PATH = "speech_emotion_recognition/features"
+IMAGES_PATH = "speech_emotion_recognition/images"
+MODELS_PATH = "speech_emotion_recognition/models"
