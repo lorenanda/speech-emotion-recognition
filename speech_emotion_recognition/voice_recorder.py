26a2abdf26e6776cf7f3a4623c32b0051ba7b9a1
lorenanda <lorena.ciutacu@gmail.com>
2020-12-17 15:19:44 +0100

    update voice_rec function

--- speech_emotion_recognition/voice_recorder.py
+++ speech_emotion_recognition/voice_recorder.py
@@ -6,12 +6,15 @@ from scipy.io.wavfile import write
 def record_voice():
     """This function records your voice and saves the output as .wav file."""
     fs = 44100  # Sample rate
-    seconds = 5  # Duration of recording
+    seconds = 3  # Duration of recording
     # sd.default.device = "Built-in Audio"  # Speakers full name here
 
+    print("Say something:")
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
     sd.wait()  # Wait until recording is finished
     write("speech_emotion_recognition/recordings/myvoice.wav", fs, myrecording)
+    print("Voice recording saved.")
 
 
-record_voice()
+if __name__ == "__main__":
+    record_voice()
