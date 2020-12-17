43ee181e5793ce3782bab30f50190219fd6a17ba
lorenanda <lorena.ciutacu@gmail.com>
2020-12-17 16:58:50 +0100

    add main function for live predictions

--- speech_emotion_recognition/main.py
+++ speech_emotion_recognition/main.py
@@ -1,3 +1,8 @@
-# -*- coding: utf-8 -*-
-
 """Main module."""
+from voice_recorder import record_voice
+from predictions import make_predictions
+
+
+def predict_myvoice():
+    record_voice()
+    make_predictions("speech_emotion_recognition/recordings/myvoice.wav")
